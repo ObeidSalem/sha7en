17fc797342a54cf50fad1dc5200f36d38108152d
Obeid Salem Ahmed Bashwar <ObeidSalem@users.noreply.github.com>
2022-09-17 22:53:54 +0800

    year

--- src/Components/CarModelPopup.js
+++ src/Components/CarModelPopup.js
@@ -6,22 +6,34 @@ import {useRef, useEffect, useState} from 'react'
 import "./Css/Popup.css"
 
 const CarModelPopup = (props) => {
-    console.log(props)
+    // console.log(props)
     // const [models1, setModels] = useState(props.vehicle.models)
-    const [width, setWidth] = useState(200);
+    
+    const [selectedModel, setSelectedModel] = useState();
+    const [width, setWidth] = useState(600);
     const carousel = useRef();
 
+    useEffect(() => {
+        // console.log(carousel.current.scrollwidth,  carousel.current.offsetWidth);
+ 
+        // setWidth(carousel.current.scrollwidth - carousel.current.offsetWidth);
+    }, []); 
 
-    const handleWidth = () => {
-        console.log(carousel.current.scrollwidth,  carousel.current.offsetWidth);
-        setWidth(carousel.current.scrollwidth - carousel.current.offsetWidth);
+    const Years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012];
+
+    function getManufactureYears() {
+        console.log("have been called");
+        Years = props.vehicle.models.filter(function (model) {
+            // console.log(model);
+            return model.modelName === "Model x";
+    }).map(function (model) {
+        console.log(model.productionYear)
+
+        return model.productionYear;
+    })
+    console.log(Years);
+
     }
-
-    // useEffect(() => {
-    //     window.addEventListener('resize', handleWidth);
-    //     console.log(carousel.current.scrollwidth)
-    // }, []); 
-
     return ( props.trigger) ?  ( 
         <div className="popup" >
             <div className="popup__container left">
@@ -36,27 +48,55 @@ const CarModelPopup = (props) => {
                             onError={(e)=>{e.target.onerror = null; e.target.src="/images/vehicle_cover_alt.jpg"}}
                     /> 
                 </div>
-                
+                <br></br>
                 <hr></hr>
+                <br></br>
                 <div className="popup__model__container">
                     {/* <h5 className="general_shadow white_bg">(((</h5> */}
-                        <motion.div className="carousel">
-                            <motion.div drag="x" dragConstraints={{right: 0, left: -width}} className="inner__carousel">
+                        <motion.div ref={carousel} className="carousel">
+                            <motion.div drag="x" dragConstraints={{right: 0, left: -width}} whileTap={{cursor: "grabbing"}} className="inner__carousel">
                                 {props.vehicle.models.map((model, i) =>{
                                 return (
-                                    <motion.div key={i}>
-                                    <img className="popup__model__image" 
-                                        src={model.image}
-                                        onError={(e)=>{e.target.onerror = null; e.target.src="/images/vehicle_cover_alt.jpg"}}
-                                        />
-                                    <h5 className="popup__model general_shadow">{model.modelName}</h5> 
-                                    </motion.div>
+                                    <motion.div 
+                                        key={model.modelName}
+                                        onClick={(e) => {
+                                            // console.log(e);
+                                            // console.log(e.target.outerText);
+                                            setSelectedModel(e.target.outerText)
+                                        }}
+                                        >
+                                        <img className="popup__model__image" 
+                                            src={model.image}
+                                            onError={(e)=>{e.target.onerror = null; e.target.src="/images/vehicle_cover_alt.jpg"}}
+                                            />
+                                        <h5 className="popup__model general_shadow">{model.modelName}</h5> 
+                                        </motion.div>
                                     );
                                 })}
                             </motion.div>
                         </motion.div>
                     {/* <h5 className="general_shadow white_bg">)))</h5> */}
                 </div>
+                <br></br>
+                {selectedModel ? 
+                <motion.div className="">
+                    <h2>Vehicle Model</h2>
+                    <div className="popup__model__container">
+                    <motion.div ref={carousel} className="carousel">
+                            <motion.div drag="x" dragConstraints={{right: 0, left: -width}} whileTap={{cursor: "grabbing"}} className="inner__carousel">
+                                {Years.map((year) =>{
+                                    return (
+                                        <motion.div >
+                                            <h5 className="popup__year general_shadow">{year}</h5> 
+                                            <br />
+                                        </motion.div>
+                                    );
+                                })}
+                            </motion.div>
+                        </motion.div>
+                    </div>
+                </motion.div>
+                : "" }
                 <br></br>
                 <button onClick={() => props.setTrigger(false)} className="close__button">CANCEL</button>
                 { props.children }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,11 +8,11 @@ const NavBar = () => {
         <nav className="navbar">
             <div className="navbar_container">
                 <ui className="nav_links">
-                    <Link className="nav_item" to="/"><it ><img className="nav_logo" src={logo} width="150" alt='logo'/></it></Link>
+                    <Link className="nav_item" to="/sha7en/"><it ><img className="nav_logo" src={logo} width="150" alt='logo'/></it></Link>
                     <div className="nav_links_left">
-                        <Link className="nav_item" to="/About"><it >About</it></Link>
-                        <Link className="nav_item" to="/Vehicles"><it >Vehicles</it></Link>
-                        <Link className="nav_item" to="/Partners"><it>Partners</it></Link>
+                        <Link className="nav_item" to="/sha7en/About"><it >About</it></Link>
+                        <Link className="nav_item" to="/sha7en/Vehicles"><it >Vehicles</it></Link>
+                        <Link className="nav_item" to="/sha7en/Partners"><it>Partners</it></Link>
                     </div>
                 </ui>
             </div>
